43c5025ed8288514d944dd9055ad641a9f06a7f5
Tony Baines <tony.baines.1@gmail.com>
2019-07-26 11:23:13 +0100

    More notes and updates

--- src/java/Demo.java
+++ src/java/Demo.java
@@ -1,14 +1,34 @@
 package java;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class Demo {
+    static String greetMany(Map<String, Integer> people) {
+        var greetings = new StringBuilder();
+        people.forEach((name, repeat) -> {
+            greetings.append("Hello ".repeat(repeat) + name);
+        });
+        return greetings.toString();
+    }
+
+    static Map people = new HashMap() {{
+        put("Tony", 3);
+        put(3, "Tony");
+    }};
+
+    enum RAG {Red, Amber, Green}
+
     public static void main(String[] args) {
-        var foo = "Foo";
 
-        Function<Integer, Integer> doubleIt = (var x) -> x * 2;
+        greetMany(people);
 
-        System.out.println(doubleIt.apply(2));
+//        var foo = "Foo";
+//
+//        Function<Integer, Integer> doubleIt = (var x) -> x * 2;
+//
+//        System.out.println(doubleIt.apply(2));
     }
 
     private String foo(var name) {
